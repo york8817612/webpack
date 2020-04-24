ff04bd339db908823281d795f4cc5eb3b77d4c7f
o黯淡o <york8817612@hotmail.com>
2020-04-24 20:58:47 +0800

    upgrade babel 7 fix vue loader error

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
